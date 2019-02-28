40f4f9ae6229ed66f60f4154314dc1c58c69704c
Kalyani Deshpande <deshpandekalyani5@gmail.com>
2019-02-28 17:47:56 -0500

    Final commit

--- Main.java
+++ Main.java
@@ -1,14 +1,7 @@
 public class Main {
 
         public static boolean isLeapYear(int year){
-            if (year  <= 0 || year > 9999){
-                return false;
-            }
-            if ( year % 100 != 0  || year  % 400 == 0)
-                return  true;
-
-              else  if(year % 4 == 0) return true;
-                return false;
+            return (year%400==0)||(year%100!=0 && year%4==0);
 
         }
         public static void main(String []args){
